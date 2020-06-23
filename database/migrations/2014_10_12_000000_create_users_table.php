2955f6376a8fb876876997a33d7fac624be51bd6
sopheaktraros <sopheaktra.ros@student.passerellesnumeriques.org>
2020-06-23 11:06:18 +0700

    step 1 to 5

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,63 @@
+<?php
+use Illuminate\Support\Str;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('role')->default(0);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        DB::table('users')->insert(
+            array(
+                'firstname'=>'admin',
+                'lastname'=>'user',
+                'email' => 'admin@example.com',
+                'position_id' =>1,
+                'role' =>1,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10)
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'firstname'=>'normal',
+                'lastname'=>'user',
+                'email' => 'normal@example.com',
+                'position_id' =>4,
+                'role' =>0,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10)
+                )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
